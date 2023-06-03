f96129cfbb7c81c66df91cb7f2484b6a68586a48
Rafael Henrique Carneiro <rafael.h@outlook.com>
2023-06-02 22:24:38 -0300

    fix: removed color test

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @rhcode/tokens
 
+## 1.1.1
+
+### Patch Changes
+
+- removed color test
+
 ## 1.1.0
 
 ### Minor Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -15,5 +15,4 @@ export const colors = {
   'ignite-500': '#00875F',
   'ignite-700': '#015F43',
   'ignite-900': '#00291D',
-  test: '#009',
 }
